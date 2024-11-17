cb81eebd7b012a373cd123f46747effd14c62fe6
Ronaldo Rodriguez <rrodriguez@kvz.cl>
2024-11-17 11:33:40 -0500

    tercer commit adding styling.

--- crowfunding/crowfunding/settings.py
+++ crowfunding/crowfunding/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'donations',
     'users',
     'django_filters',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -128,7 +130,9 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
